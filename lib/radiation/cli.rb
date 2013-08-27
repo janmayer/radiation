e30be4071a143849948e10f98ea5ea08fb7b47fb
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2013-08-27 11:05:30 +0200

    Changed number formatting in CLI, see plusminus nio_write

--- lib/radiation/cli.rb
+++ lib/radiation/cli.rb
@@ -16,7 +16,7 @@ class CLI < Thor
 	def source(nuclide)
 		resource = options[:resource] ? options[:resource] : "iaea"
 		puts ["E_ɣ", "ΔE_ɣ", "I_ɣ", "ΔI_ɣ"].join("\t")
-		puts Radiation::Source.new(nuclide: nuclide, resource: resource).intensities.collect{|l| [l[:energy].value, l[:energy].delta, l[:intensity].value, l[:intensity].delta].join("\t") }
+		puts Radiation::Source.new(nuclide: nuclide, resource: resource).intensities.collect{|l| [l[:energy].nio_write, l[:energy].delta, l[:intensity].nio_write, "%.0E" % l[:intensity].delta].join("\t") }
 	end
 
 	desc "resources", "List available data resources"
@@ -56,7 +56,7 @@ class CLI < Thor
 		spectrum = Radiation::Spectrum.new(source: source ).parse_hdtv(file)
 		puts ["E_ɣ", "I_ɣ", "ΔI_ɣ", "e", "Δe"].join("\t")
 		spectrum.calibrate.efficiencies.peaks.select{|p| p[:intensity] > mini}.sort_by{|k| k[:energy]}.each do |p|
-			puts [ p[:energy].to_f.round(1), p[:intensity].value, p[:intensity].delta, p[:efficiency].value.round(1), p[:efficiency].delta.round(1) ].join("\t")
+			puts [ p[:energy].to_f.round(1), p[:intensity].nio_write, "%.0E" % p[:intensity].delta, p[:efficiency].nio_write, "%.0E" % p[:efficiency].delta ].join("\t")
 		end
 	end
 
